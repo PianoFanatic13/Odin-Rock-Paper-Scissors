714374c73030f77211d8c659a0bc9d8607a43e2d
PianoFanatic13 <abdulraqib0224@gmail.com>
2023-02-23 23:47:34 -0800

    Got buttons to work after stupid bugs
    The buttons work and run the game, but still in the console

--- javascript.js
+++ javascript.js
@@ -1,83 +1,112 @@
-const arr = ["Rock", "Paper", "Scissors"];
+window.addEventListener('load', () => {
+    
+    const arr = ["Rock", "Paper", "Scissors"];
 
-
-function getComputerChoice(arr)
-{
-let x =  Math.floor(Math.random() * 3);
-return arr[x];
-}
-
-function getPlayerChoice(option_arr)
-{
-    let playerInput;
-    let newInput;
-
-    do
+    function getComputerChoice()
     {
-        playerInput = prompt("Rock, Paper, or Scissors?: \n");
-        newInput = playerInput.charAt(0).toUpperCase() + playerInput.toLowerCase().slice(1);
-    } while (arr.includes(newInput) == false)
-
-    return newInput;
-}
-
-function playRound(pChoice, cChoice)
-{
-    //Player win
-    if ((pChoice == "Rock" && cChoice == "Scissors") || 
-    (pChoice == "Scissors" && cChoice == "Paper") ||
-    (pChoice == "Paper" && cChoice == "Rock"))
-    {
-        console.log("Player Wins!\n");
-        return 1;
+    let x =  Math.floor(Math.random() * 3);
+    
+    if (x == 0) {
+        return "Rocks";
     }
-    else if (pChoice == cChoice) //Tie
-    {
-        console.log("Tie!\n");
-        return 0;
+    else if (x == 1) {
+        return "Paper";
     }
-    else //Computer wins
-    {
-        console.log("Computer Wins!\n");
-        return -1;
+    else {
+        return "Scissors";
     }
-}
+    }
 
-function playGame(arr)
-{
-    let cChoice;
-    let pChoice;
-    let winLoss;
-    let pWins;
-    let cWins;
-
-    for (let i = 1; i < 6; i++)
+    function getPlayerChoice(option_arr)
     {
-        console.log("Round: " + i);
-        cChoice = getComputerChoice(arr);
-        pChoice = getPlayerChoice(arr);
+        let playerInput;
+        let newInput;
 
-        console.log(`Player Choice: ${pChoice}\nComputer Choice: ${cChoice}\n`)
-        winLoss = playRound(pChoice, cChoice);
-
-        switch(winLoss)
+        do
         {
-            case 1:
-                pWins++;
-                break;
-            case -1:
-                cWins;
-                break;
+            playerInput = prompt("Rock, Paper, or Scissors?: \n");
+            newInput = playerInput.charAt(0).toUpperCase() + playerInput.toLowerCase().slice(1);
+        } while (arr.includes(newInput) == false)
+
+        return newInput;
+    }
+
+    function playRound(pChoice, cChoice)
+    {
+        //Player win
+        console.log(`Player Choice: ${pChoice}\nComputer Choice: ${cChoice}\n`);
+        if ((pChoice == "Rock" && cChoice == "Scissors") || 
+        (pChoice == "Scissors" && cChoice == "Paper") ||
+        (pChoice == "Paper" && cChoice == "Rock"))
+        {
+            console.log("Player Wins!\n");
+            return 1;
+        }
+        else if (pChoice == cChoice) //Tie
+        {
+            console.log("Tie!\n");
+            return 0;
+        }
+        else //Computer wins
+        {
+            console.log("Computer Wins!\n");
+            return -1;
         }
     }
-    if (pWins > cWins)
-    {
-        console.log("Player Wins the Entire Game!\n");
-    }
-    else
-    {
-        console.log("Computer Wins the Entire Game!\n");
-    }
-}
 
-playGame(arr);
+    function playGame(arr)
+    {
+        let cChoice;
+        let pChoice;
+        let winLoss;
+        let pWins;
+        let cWins;
+
+        for (let i = 1; i < 6; i++)
+        {
+            console.log("Round: " + i);
+            cChoice = getComputerChoice();
+            pChoice = getPlayerChoice(arr);
+
+            console.log(`Player Choice: ${pChoice}\nComputer Choice: ${cChoice}\n`);
+            winLoss = playRound(pChoice, cChoice);
+
+            switch(winLoss)
+            {
+                case 1:
+                    pWins++;
+                    break;
+                case -1:
+                    cWins;
+                    break;
+            }
+        }
+        if (pWins > cWins)
+        {
+            console.log("Player Wins the Entire Game!\n");
+        }
+        else
+        {
+            console.log("Computer Wins the Entire Game!\n");
+        }
+    }
+
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
+
+    rock.addEventListener('click', () => {
+        playRound("Rock", getComputerChoice());
+    })
+    
+    paper.addEventListener('click', () => {
+        playRound("Paper", getComputerChoice());
+    });
+    
+    scissors.addEventListener('click', () => {
+        playRound("Scissors", getComputerChoice())
+    });
+})
+
+//playGame(arr);
+
